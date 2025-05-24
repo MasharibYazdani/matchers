6fab4ae12d5042939fc675b4d733f6d78263333c
Masharib Yazdani <[masharibyazdani@gmail.com]>
2025-05-24 11:15:12 +0530

    routing setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+
 function App() {
   return (
     <>
-      <h1 className="text-6xl">Hello World</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
